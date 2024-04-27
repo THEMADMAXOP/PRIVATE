c5bf225b3c801291a0129483bef5dc93c0ce6bf9
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-04-27 13:33:14 +0530

    Update start.py

--- ARYAN/utils/inline/start.py
+++ ARYAN/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
             
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="languages"),
         ],
     ]
     return buttons
