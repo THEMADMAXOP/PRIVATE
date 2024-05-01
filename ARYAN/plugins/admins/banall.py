5cb2a1616528e8a5a76eb4d7419880d0d8da00ce
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-05-01 20:33:35 +0530

    Update banall.py

--- ARYAN/plugins/admins/banall.py
+++ ARYAN/plugins/admins/banall.py
@@ -7,7 +7,7 @@ from ARYAN.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
 
-@app.on_message(filters.command("madmaxbanall") & SUDOERS)
+@app.on_message(filters.command("aryanopxd") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id    
     bot = await app.get_chat_member(chat_id, BOT_ID)
