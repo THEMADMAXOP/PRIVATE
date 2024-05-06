fbaf7740e1e29e3e024f33676dc6cbef07ef7f75
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-05-06 07:25:23 +0530

    Update play.py

--- ARYAN/utils/inline/play.py
+++ ARYAN/utils/inline/play.py
@@ -3,6 +3,7 @@ import math
 from pyrogram.types import InlineKeyboardButton
 
 from ARYAN.utils.formatters import time_to_seconds
+from ARYAN.plugins.bot.settings import lood2
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
