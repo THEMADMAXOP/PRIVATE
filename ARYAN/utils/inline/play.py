a167738e5b39986496f4a665155da47b6c4b8657
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-05-06 07:43:13 +0530

    Update play.py

--- ARYAN/utils/inline/play.py
+++ ARYAN/utils/inline/play.py
@@ -106,11 +106,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                    text=_["S_B_9"],
-                    callback_data="lood2"),
-                ],
+    
     ]
     return buttons
 
