8a614cfb97c2cdad2096c737da89da1f9c7bdb15
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-05-28 09:09:30 +0530

    Update settings.py

--- ARYAN/plugins/bot/settings.py
+++ ARYAN/plugins/bot/settings.py
@@ -35,7 +35,6 @@ from ARYAN.utils.inline.settings import (
     setting_markup,
     vote_mode_markup,
 )
-from ARYAN.utils.inline import source_markup, lood_markup
 from ARYAN.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID, MUSIC_BOT_NAME, START_IMG_URL
 
